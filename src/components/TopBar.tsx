816e233492a3a924fb49a2646df7c521963707d1
José Galdámez <josegaldamez1991@gmail.com>
2024-11-13 13:07:20 -0600

    change title again

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,7 @@
 export const TopBar = () => {
     return (
-        <header className="bg-blue-300 text-blue-900 p-5 text-center">
-            <h1 className="font-thin">Mini proyectos de <span className="font-bold"> José Ramos</span></h1>
+        <header className="bg-blue-400 text-blue-900 p-5 text-center">
+            <h1 className="font-thin"><span className="text-white">Mini proyectos de</span> <span className="font-bold"> José Galdámez</span></h1>
         </header>
     )
 }
